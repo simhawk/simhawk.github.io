fedc44546a86c37fccb9c7cda1cea2d30bc1324c
simhawk <isaiah.erb@gmail.com>
2019-07-31 16:22:42 -0400

    finilized structure, just change minor styling, add content

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -11,28 +11,52 @@
 var cbpAnimatedHeader = (function() {
 
 	var docElem = document.documentElement,
-		header = document.querySelector( '.navbar-fixed-top' ),
+		navFixedTop = document.querySelector( '.navbar-fixed-top' ),
+		navbar = document.querySelector('.navbar'),
+		projectsTitle = document.querySelector('.projects-title'),
+		links = navbar.getElementsByTagName("a"),
 		didScroll = false,
+		lastScroll = 0,
 		changeHeaderOn = 300;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
 			if( !didScroll ) {
 				didScroll = true;
-				setTimeout( scrollPage, 250 );
+				setTimeout( scrollPage, 100 );
 			}
 		}, false );
 	}
 
 	function scrollPage() {
+		if (document.documentElement.scrollTop === 0) {
+			for(var i = 0; i < links.length; i++){
+				classie.remove(links[i],"thing")	
+			}
+		} else {
+			for(var i = 0; i < links.length; i++){
+				classie.add(links[i],"thing")	
+			}
+		}
+		/*
 		var sy = scrollY();
 		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
+			classie.add( navFixedTop, 'navbar-shrink' );
 		}
 		else {
-			classie.remove( header, 'navbar-shrink' );
+			classie.remove( navFixedTop, 'navbar-shrink' );
 		}
+
+		// scrolling down
+		if(sy - lastScroll < 0) {
+			classie.add(navbar, '.navbar-transparent');
+		} else {
+			classie.remove(navbar, '.navbar-transparent');
+		}
+		lastScroll = sy;
+		*/
 		didScroll = false;
+
 	}
 
 	function scrollY() {
