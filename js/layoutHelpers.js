0032b4a355b3b7dcdeaec5d6b7835c9e8fed427a
simhawk <isaiah.erb@gmail.com>
2019-08-21 18:43:27 -0400

    finished????

--- js/layoutHelpers.js
+++ js/layoutHelpers.js
@@ -1,3 +1,23 @@
-var layoutHelpers = (function() {
+
+$(document).ready(function() {
+
+    for(var i = 1; i <= 18; i++) {
+        var modalID = '#portfolioModal' + i;
+        if(window.location.href.indexOf(modalID) != -1) {
+            $(modalID).modal('show');
+        }
+    }
+
+    if(window.location.href.indexOf('#UAV') != -1) {
+        $("#portfolioModal5").modal('show');
+    }
+
+    if(window.location.href.indexOf('#Elixr') != -1) {
+        $('#portfolioModal4').modal('show');
+    }
+
+    if(window.location.href.indexOf('#MLRoad') != -1) {
+        $('#portfolioModal3').modal('show');
+    }
     
-})();
+});
