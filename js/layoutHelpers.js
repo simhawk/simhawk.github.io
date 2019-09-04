93bdff5f2b54e9d9ec9f5e991395c0caad389fc9
simhawk <isaiah.erb@gmail.com>
2019-09-04 17:09:07 -0400

    added delay for modal popups, added new link for different resume

--- js/layoutHelpers.js
+++ js/layoutHelpers.js
@@ -15,24 +15,67 @@ $(document).ready(function() {
 
     if(window.location.href.indexOf('#digi') != -1) {
         $("#portfolioModal1").modal('show');
+        $('body,html').animate(
+            {
+                scrollTop: $('#portfolioThumb1').offset().top
+            },
+            1200
+        );
     }
 
     if(window.location.href.indexOf('#UAV') != -1) {
         $("#portfolioModal5").modal('show');
+        $('body,html').animate(
+            {
+                scrollTop: $('#portfolioThumb5').offset().top
+            },
+            1200
+        );
     }
 
     if(window.location.href.indexOf('#Elixr') != -1) {
         $('#portfolioModal4').modal('show');
+        $('body,html').animate(
+            {
+                scrollTop: $('#portfolioThumb4').offset().top
+            },
+            1200
+        );
     }
 
     if(window.location.href.indexOf('#MLRoad') != -1) {
         $('#portfolioModal3').modal('show');
+        $('body,html').animate(
+            {
+                scrollTop: $('#portfolioThumb3').offset().top
+            },
+            1200
+        );
     }
 
+    if(window.location.href.indexOf('#SLAM') != -1) {
+        $('#portfolioModal2').modal('show');
+        $('body,html').animate(
+            {
+                scrollTop: $('#portfolioThumb2').offset().top
+            },
+            1200
+        );
+    }
+
+    $('.modal').on('hidden.bs.modal', function(e)
+    { 
+        $(this).removeData();
+    }) ;
+    
     
     /* Onclicks */
+    var prevDisabled = false;
     $('.btn-prev').on('click', function(event) {
 
+        if(prevDisabled) {
+            return;
+        }
         //close modal
         $('.modal').modal('hide');
         $('.modal-backdrop').remove()
@@ -45,10 +88,20 @@ $(document).ready(function() {
         var newModalID = '#portfolioModal' + (newNum+1);
 
         $(newModalID).modal('show');
+
+        //disable button on timeout
+        prevDisabled = true;
+        setTimeout(function(){
+            prevDisabled = false;
+        }, 500);
     });
 
+    var nextDisabled = false;
     $('.btn-next').on('click', function(event) {
 
+        if(nextDisabled) {
+            return;
+        }
         //cloase modal
         $('.modal').modal('hide');
         $('.modal-backdrop').remove()
@@ -62,6 +115,12 @@ $(document).ready(function() {
 
         //close then open
         $(newModalID).modal('show');
+        
+        //disable button on timeout
+        nextDisabled = true;
+        setTimeout(function(){
+            nextDisabled = false;
+        }, 500);
     });
     
 });
